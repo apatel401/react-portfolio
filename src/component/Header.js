a9a099346f098036ce3208673b87eb64a1bc8130
Akash Patel <apatel@tvo.org>
2022-03-22 22:50:01 -0400

    finalized dark theme

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,22 +1,34 @@
-import React from "react";
-import About from "./About";
+import React, { useContext} from "react";
+// import About from "./About";
 import Projects from "./Projects";
 import Contact from "./Contact";
 import {FcContacts, FcDocument, FcLightAtTheEndOfTunnel, FcHome} from "react-icons/fc"
 
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Resume from "./Resume";
 import Home from "./Home";
 import Skills from "./Skills";
+import {FaSun, FaMoon} from "react-icons/fa";
+import { ThemeContext } from "./Provider";
 
 
 const Header = () => {
+  const context = useContext(ThemeContext);
+console.log(context);
+  function switchTheme() {
+    const newTheme = context.theme === "dark" ? "light" : "dark";
+    context.updateContext({ theme: newTheme });
+  }
+
   return (
     <Router>
       <Navbar collapseOnSelect expand="lg">
-        <Container>
+      <div className="container">
           <Navbar.Brand>Akash Patel</Navbar.Brand>
+          <div className="themeChange" onClick={switchTheme}>
+              {context.theme === "dark" ? <FaSun /> : <FaMoon />}
+              </div>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav>
@@ -37,7 +49,7 @@ const Header = () => {
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
-        </Container>
+      </div>        
       </Navbar>
       <Switch>
         <Route path="/" exact>
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,63 +1,61 @@
-import React, {useRef, useState, useEffect, useContext} from 'react'
-import Lottie from 'lottie-react' 
-import testAnimation from './assets/skills.json'
+import React, { useRef, useState, useEffect, useContext } from "react";
+import Lottie from "lottie-react";
+import testAnimation from "./assets/skills.json";
 
-import 'splitting/dist/splitting.css'
-import 'splitting/dist/splitting-cells.css'
-import Splitting from 'splitting'
-import ToggleButton from 'react-bootstrap/ToggleButton'
-import { ThemeContext } from './Provider'
+import "splitting/dist/splitting.css";
+import "splitting/dist/splitting-cells.css";
+import Splitting from "splitting";
+// import { ThemeContext } from "./Provider";
 
 export default function Home() {
-  const [lines, setLines] = useState([])
-  const splitRef = useRef(null)
-  const context = useContext(ThemeContext)
+  const [lines, setLines] = useState([]);
+  const splitRef = useRef(null);
+  // const context = useContext(ThemeContext);
 
   useEffect(() => {
     if (splitRef) {
       let split_res = Splitting({
-        by: 'chars',
-        whitespace: true
+        by: "chars",
+        whitespace: true,
       });
-      console.log(split_res[0].chars);
       setLines(split_res[0].chars);
-      console.log(lines);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [splitRef])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [splitRef]);
 
   const defaultOptions = {
     loop: true,
     autoplay: true,
     rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice"
-    }
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
-  
-  function switchTheme(){
-    const newTheme = context.theme === 'dark' ? 'light' : 'dark';
-    context.updateContext({theme: newTheme})
-  }
-  
 
-    return (
-      <div className="container-fluid home">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-6 col-xs-12">
-            <Lottie option={defaultOptions} animationData={testAnimation} loop={true} />
-            </div>
-            <div className="col-sm-6 col-xs-12 d-flex justify-content-center align-items-center">
-              <h2 ref={splitRef}
-      data-splitting='true'>
-                I am a front-end Developer
-                On verge of Becoming
-                Fullstack Developer
-              </h2>
-              <ToggleButton onClick={switchTheme} >switch theme</ToggleButton>
+  // function switchTheme() {
+  //   const newTheme = context.theme === "dark" ? "light" : "dark";
+  //   context.updateContext({ theme: newTheme });
+  // }
+
+  return (
+    <div className="container-fluid home">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-xs-12">
+            <Lottie
+              option={defaultOptions}
+              animationData={testAnimation}
+              loop={true}
+            />
+          </div>
+          <div className="col-sm-6 col-xs-12 d-flex justify-content-center align-items-center">
+            <h2 ref={splitRef} data-splitting="true">
+              I am a front-end Developer On verge of Becoming Fullstack
+              Developer
+            </h2>
+            
             </div>
           </div>
         </div>
       </div>
-    );
+  );
 }
