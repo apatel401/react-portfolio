573e5f4105b8fb7c13b775aad2ea9c3f96e9b5a6
Akash Patel <apatel@tvo.org>
2022-03-30 23:20:29 -0400

    Resume page setup, floating menu setup

---
+++ src/component/FloatingMenu.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import { ImFolderDownload } from "react-icons/im";
+import { AiOutlineMail, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
+import Tooltip from "./Tooltip";
+import { Link } from "react-router-dom";
+
+function FloatingMenu() {
+  return (
+    <ul className="floating-menu">
+    <li>
+      <Tooltip
+        content={
+          <>
+            <span>Download Resume</span>
+          </>
+        }
+        direction="right"
+        delay="0">
+        <button>
+          <ImFolderDownload />
+        </button>
+      </Tooltip>
+    </li>
+    <li>
+      <Tooltip
+        content={
+          <>
+            <span>Download Resume</span>
+          </>
+        }
+        direction="right"
+        delay="0">
+        <button as={Link} to={{ pathname: "###" }} target="_blank">
+          <AiFillLinkedin />
+        </button>
+      </Tooltip>
+    </li>
+    <li>
+      <Tooltip
+        content={
+          <>
+            <span>Download Resume</span>
+          </>
+        }
+        direction="right"
+        delay="0">
+        <button>
+          <AiFillGithub />
+        </button>
+      </Tooltip>
+    </li>
+    <li>
+      <Tooltip
+        content={
+          <>
+            <span>Download Resume</span>
+          </>
+        }
+        direction="right"
+        delay="0">
+        <button>
+          <AiOutlineMail />
+        </button>
+      </Tooltip>
+    </li>
+    <li>
+      <Tooltip
+        content={
+          <>
+            <span>Download Resume</span>
+          </>
+        }
+        direction="right"
+        delay="0"></Tooltip>
+    </li>
+  </ul>
+  )
+}
+
+export default FloatingMenu
--- src/component/Resume.js
+++ src/component/Resume.js
@@ -1,47 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import FloatingMenu from "./FloatingMenu";
+import { JobData } from "./JobData";
 
 function Resume() {
+  // const [jobs, setJobs] = useState(JobData);
+  const [value, setValue] = useState(0);
+const {title, company, duties, dates } = JobData[value];
   return (
-
-          <div id="resume" className="container mt-5">
-            <div className="card mb-3">
-              <div className="row">
-                <div className="col-md-10">
-                  <div className="card-body">
-                    <h5 className="card-title">Brief</h5>
-                    <p className="card-text"> A creative professional with 3 years of hands on for web &amp; mobile application. I am a multi-tasking web
-                      designer with passion for designing clean and functional websites, web applications &amp; development along with print media. I amWeb developer 
-                      with experience coding  websites and web application using Javascript, React, Angular, NPM, NodeJS, HTML, CSS, wordPress, javascript, jquary, 
-                      Bootstrap. I focus on writing clear and efficient codes. Pixel perfect creation &amp; attention to details are my strengths 
-                      and I believe in quality oriented results. Currently residing in Oshawa, ON, Canada. I am looking to grow professionally and personally.</p>
-                  </div>
-                </div>
-                <div className="col-md-2">
-                  <img
-                    src="./akash.jpeg"
-                    className="img-fluid rounded-start"
-                    alt="..."
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <ul>
-                <li>
-                  <button>Download Resume</button>
-                </li>
-                <li>
-                  <button>LinkedIn</button>
-                </li>
-                <li>
-                  <button>Github</button>
-                </li>
-                <li>
-                  <button>Gmail</button>
-                </li>
-              </ul>
+    <div id="resume" className="container mt-5">
+      <h1 className="text-center">Resume</h1>
+      <div className="card mb-3">
+        <div className="row">
+          <div className="col-md-10">
+            <div className="card-body">
+              <h2 className="card-title">Profile</h2>
+              <p className="card-text">
+                A creative professional with 3 years of hands on for web &amp;
+                mobile application. I am a multi-tasking web designer with
+                passion for designing clean and functional websites, web
+                applications &amp; development along with print media. I amWeb
+                developer with experience coding websites and web application
+                using Javascript, React, Angular, NPM, NodeJS, HTML, CSS,
+                wordPress, javascript, jquary, Bootstrap. I focus on writing
+                clear and efficient codes. Pixel perfect creation &amp;
+                attention to details are my strengths and I believe in quality
+                oriented results. Currently residing in Oshawa, ON, Canada. I am
+                looking to grow professionally and personally.
+              </p>
             </div>
           </div>
+          <div className="col-md-2">
+            <img
+              src="./akash.jpeg"
+              className="img-fluid rounded-start"
+              alt="..."
+            />
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="row">
+        <FloatingMenu />
+      </div>
+      <div className="row">
+        <h2 className="">Experience</h2>
+        <div className="ex-container">
+          {JobData.map((job, index) => {
+            return (
+                <div className="title-container">
+                  <button key={job.id} id={job.id} 
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}>{job.company}</button>
+                </div>
+                 );
+                })}
+                <div className="details-container">
+                  <h3>{title}</h3>
+                  {dates.split(",").length > 1 ? (
+                    <>
+                      <h4>{dates.split(",")[0]}</h4>
+                      <h4>{dates.split(",")[1]}</h4>
+                    </>
+                  ) : (
+                    <h4>{dates}</h4>
+                  )}
+                  {duties.map((duty) => {
+                    return <p>{duty}</p>;
+                  })}
+                </div>
+           
+        </div>
+      </div>
+    </div>
   );
 }
 
