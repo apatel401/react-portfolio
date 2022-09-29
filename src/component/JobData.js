7ec91aa47854d7d8118646149556116051ede27c
apatel401 <51169932+apatel401@users.noreply.github.com>
2022-09-28 23:59:11 -0400

    more details added for modified project page

--- src/component/JobData.js
+++ src/component/JobData.js
@@ -1,128 +1,178 @@
 export const JobData = [
-    {
-      "id": "1",
-      "company": "TVO",
-      "title": "web Production Artist",
-      "dates": "December 2020 - Present, September 2019 - June 2019",
-      "duties": [
-        "Developing and maintaining automation scripts which can optimize tasks.",
-        "Developing and maintaining reusable Interactive Learning Objects and widgets.",
-        "Producing vendor files using NPM, Node.js, Webpack, Grunt and Gulp bundling tools",
-        "Troubleshooting the bugs reported by internal QA team or external team.", 
-        "Finding solution globally which can improve AODA of course project.",
-        "Responsible for taking care of W3C and WCAG 2.0 (Level AA) and AODA Compliance.",
-        "Improving web accessibility of courses and developed scripts to automate some of the work."
-      ]
-    },
-    {
-      "id": "2",
-      "company": "Taqqut productions",
-      "title": "web developer",
-      "dates": "June 2020 - December 2020",
-      "duties": [
-        "Design customized theme using WordPress.",
-        "Design customized theme for Shopify. Shopify code edit expert.",
-        "Developing Game using HTML5 Canvas, CSS, JavaScript and jQuery.",
-        "Responsible for testing website, Forms included in the website.", 
-        "Following Designer's Guideline to Develop responsive website.",
-        "Maintaining all W3C standards WCAG 2.0 and AODA standards."
-      ]
-    },
-    {
-      "id": "3",
-      "company": "BoldPod Inc.",
-      "title": "Front-end developer",
-      "dates": "January 2019 - September 2019",
-      "duties": [
-        "Design customized theme using WordPress",
-        "Develop an app using phone gap for Android OS and IOS.",
-        "Design of great visual identity that are SEO Friendly.",
-        "Maintaining AODA and W3C standard in all website developed.", 
-        "Design professional PPT, Mockups, Blog graphics, Infographics",
-        "Proficiency in Creative Design Software specifically Photoshop."
-      ]
-    }
-  ];
+  {
+    id: "1",
+    company: "TVO",
+    title: "web Production Artist",
+    dates: "December 2020 - Present, September 2019 - June 2019",
+    duties: [
+      "Developing and maintaining automation scripts which can optimize tasks.",
+      "Developing and maintaining reusable Interactive Learning Objects and widgets.",
+      "Producing vendor files using NPM, Node.js, Webpack, Grunt and Gulp bundling tools",
+      "Troubleshooting the bugs reported by internal QA team or external team.",
+      "Finding solution globally which can improve AODA of course project.",
+      "Responsible for taking care of W3C and WCAG 2.0 (Level AA) and AODA Compliance.",
+      "Improving web accessibility of courses and developed scripts to automate some of the work.",
+    ],
+  },
+  {
+    id: "2",
+    company: "Taqqut productions",
+    title: "web developer",
+    dates: "June 2020 - December 2020",
+    duties: [
+      "Design customized theme using WordPress.",
+      "Design customized theme for Shopify. Shopify code edit expert.",
+      "Developing Game using HTML5 Canvas, CSS, JavaScript and jQuery.",
+      "Responsible for testing website, Forms included in the website.",
+      "Following Designer's Guideline to Develop responsive website.",
+      "Maintaining all W3C standards WCAG 2.0 and AODA standards.",
+    ],
+  },
+  {
+    id: "3",
+    company: "BoldPod Inc.",
+    title: "Front-end developer",
+    dates: "January 2019 - September 2019",
+    duties: [
+      "Design customized theme using WordPress",
+      "Develop an app using phone gap for Android OS and IOS.",
+      "Design of great visual identity that are SEO Friendly.",
+      "Maintaining AODA and W3C standard in all website developed.",
+      "Design professional PPT, Mockups, Blog graphics, Infographics",
+      "Proficiency in Creative Design Software specifically Photoshop.",
+    ],
+  },
+];
 
-  export const School = [
-{
-  "id": "1",
-  "name": "Seneca College",
-  "courseName": "Javascript and Angular",
-  "year": "May 2019 - August 2019"
-},
-{
-  "id": "2",
-  "name": "Gujarat University",
-  "courseName": "Bachelor of Computer Applications",
-  "year": "May 2019 - August 2019"
-}
-  ];
-  export const CompDisplay = [
-    {
-      "name": "Dashboard",
-      "liveUrl": "https://dapper-dashboard.netlify.app/",
-      "techStack" : "React, NodeJS, Sass, Webpack"
-    },
-    {
-      "name": "Project Management",
-      "liveUrl": "",
-      "techStack" : "GraphQL, MongoDB, Express, React, NodeJS, Webpack"
-    },
-    {
-      "name": "Component Library",
-      "liveUrl": "https://apatelportfolio.netlify.app/component-library",
-      "techStack" : "React, NodeJS, Webpack, Gulp,"
-    },
-    {
-      "name": "multiple-choice",
-      "liveUrl": "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/multiple-choice.html",
-      "techStack" : "React, NodeJS, Sass, Webpack, Gulp"
-    },
-    {
-      "name": "multiple choice checkbox",
-      "liveUrl": "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/compound-interest-calculator.html",
-      "techStack" : "React, NodeJS, Sass, Webpack, Gulp"
-    },
-    {
-      "name": "Image hotspot",
-      "liveUrl": "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/interactive-images.html",
-      "techStack" : "React, NodeJS, Sass, Webpack, Gulp"
-    },
-    {
-      "name": "Flashcards",
-      "liveUrl": "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/flashcards.html",
-      "techStack" : "React, NodeJS, Sass, Webpack, Gulp"
-    },
-    {
-      "name": "matching widget",
-      "liveUrl": "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/matching.html",
-      "techStack" : "React, NodeJS, Sass, Webpack, Gulp"
-    },
-    {
-      "name": "Content cabinet",
-      "liveUrl": "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/content-cabinet.html",
-      "techStack" : "React, NodeJS, Sass, Webpack, Gulp"
-    },
-    {
-      "name": "flowchart",
-      "liveUrl": "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/flowchart.html",
-      "techStack" : "React, NodeJS, Sass, Webpack, Gulp"
-    },
-    {
-      "name": "sorting table",
-      "liveUrl": "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/sorting.html",
-      "techStack" : "React, NodeJS, Sass, Webpack, Gulp"
-    },
-    {
-      "name": "Image juxtaposition",
-      "liveUrl": "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/image-juxtaposition.html",
-      "techStack" : "React, NodeJS, Sass, Webpack, Gulp"
-    },
-    {
-      "name": "Interest calculator",
-      "liveUrl" : "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/compound-interest-calculator.html",
-      "techStack" : "React, NodeJS, Sass, Webpack, Gulp"
-    },
-
-  ]
+export const School = [
+  {
+    id: "1",
+    name: "Seneca College",
+    courseName: "Javascript and Angular",
+    year: "May 2019 - August 2019",
+  },
+  {
+    id: "2",
+    name: "Gujarat University",
+    courseName: "Bachelor of Computer Applications",
+    year: "May 2019 - August 2019",
+  },
+];
+export const CompDisplay = [
+  {
+    name: "Dashboard",
+    liveUrl: "https://dapper-dashboard.netlify.app/",
+    appDetail:
+      "React Admin Dashboard Application. This Admin Panel includes one Dashboard, Three Pages, Four Apps, and Seven fully functional charts! using SyncFusion components",
+    imgSrc: "",
+    code: "https://github.com/apatel401/dashboard",
+    techStack: "React, NodeJS, Webpack, Tailwind UI, SyncFusion",
+  },
+  {
+    name: "Project Management",
+    liveUrl: "",
+    appDetail:
+      "full-stack project management system with GraphQL, Express, MongoDB, React, and Apollo.",
+    imgSrc: "",
+    code: "https://github.com/apatel401/AG-MERN-Project-management",
+    techStack: "Apollo, GraphQL, MongoDB, Express, React, NodeJS, Webpack",
+  },
+  {
+    name: "Component Library",
+    liveUrl: "https://apatelportfolio.netlify.app/component-library",
+    appDetail: "Library made of reusable component made using React, NodeJs , npm Registry, Webpack and Gulp",
+    imgSrc: "",
+    code: "",
+    techStack: "React, NodeJS, Webpack, Gulp, NPM",
+  },
+  {
+    name: "multiple-choice",
+    liveUrl:
+      "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/multiple-choice.html",
+    appDetail: "",
+    imgSrc: "",
+    code: "",
+    techStack: "React, NodeJS, Sass, Webpack, Gulp",
+  },
+  // {
+  //   name: "multiple choice checkbox",
+  //   liveUrl:
+  //     "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/compound-interest-calculator.html",
+  //   appDetail: "",
+  //   imgSrc: "",
+  //   code: "",
+  //   techStack: "React, NodeJS, Sass, Webpack, Gulp",
+  // },
+  {
+    name: "Interactive Images",
+    liveUrl:
+      "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/interactive-images.html",
+    appDetail: "",
+    imgSrc: "/img/interactive-images.jpg",
+    code: "",
+    techStack: "React, NodeJS, Sass, Webpack, Gulp",
+  },
+  {
+    name: "Flashcards",
+    liveUrl:
+      "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/flashcards.html",
+    appDetail: "",
+    imgSrc: "/img/flashcards.jpg",
+    code: "",
+    techStack: "React, NodeJS, Sass, Webpack, Gulp",
+  },
+  {
+    name: "matching widget",
+    liveUrl:
+      "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/matching.html",
+    appDetail: "",
+    imgSrc: "",
+    code: "",
+    techStack: "React, NodeJS, Sass, Webpack, Gulp",
+  },
+  {
+    name: "Content cabinet",
+    liveUrl:
+      "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/content-cabinet.html",
+    appDetail: "",
+    imgSrc: "",
+    code: "",
+    techStack: "React, NodeJS, Sass, Webpack, Gulp",
+  },
+  {
+    name: "flowchart",
+    liveUrl:
+      "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/flowchart.html",
+    appDetail: "",
+    imgSrc: "",
+    code: "",
+    techStack: "React, NodeJS, Sass, Webpack, Gulp",
+  },
+  {
+    name: "sorting table",
+    liveUrl:
+      "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/sorting.html",
+    appDetail: "",
+    imgSrc: "",
+    code: "",
+    techStack: "React, NodeJS, Sass, Webpack, Gulp",
+  },
+  {
+    name: "Image juxtaposition",
+    liveUrl:
+      "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/image-juxtaposition.html",
+    appDetail: "",
+    imgSrc: "",
+    code: "",
+    techStack: "React, NodeJS, Sass, Webpack, Gulp",
+  },
+  {
+    name: "Interest calculator",
+    liveUrl:
+      "https://content-solutions.s3.ca-central-1.amazonaws.com/courseware/showcase/k8-interactives/lessons/compound-interest-calculator.html",
+    appDetail: "",
+    imgSrc: "",
+    code: "",
+    techStack: "React, NodeJS, Sass, Webpack, Gulp",
+  },
+];
