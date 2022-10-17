d0bf5d84348f5b0b0d519b7018a289b0bd8e93c4
apatel401 <51169932+apatel401@users.noreply.github.com>
2022-10-17 01:00:38 -0400

    One more app added

--- src/component/ComponentLibrary.js
+++ src/component/ComponentLibrary.js
@@ -60,20 +60,6 @@ export default function ComponentLibrary() {
 
   return (
     <>
-      {/* <div className="main-card-wrapper">
-      <a href="#one">
-      <div className="card">
-      <img src="./akash.jpeg" alt="" className="card-image" />
-      <div className="card-overlay">
-        <div className="card-header">
-      <p className="card-title">General Production</p>
-      </div>
-      <p className="card-readnow">Read Now</p>
-      </div>
-      </div>
-      </a>
-      </div>
-      */}
       <div className="comp-wrapper">
       <ul className="component-btn">
         {CompDisplayLibrary.map((component) => {
@@ -84,20 +70,6 @@ export default function ComponentLibrary() {
         {pickComponent()}
       </div>
       </div>
-
-      {/* <MultipleChoiceComponent
-        config="./widgets/multiple-choice-component/multiple-choice-component-1.json"
-        id="1"
-      />
-      <DragDropMatching
-        config="./widgets/drag-and-drop-matching/matching.json"
-        id="2"
-      />
-      <Flowchart config="./widgets/flowchart/flowchart2.json" id="3" />
-      <Flashcards config="./widgets/flashcards/config.json" id="4" />
-      <Flashcards config="./widgets/flashcards/config2.json" id="5" />
-      <Flashcards config="./widgets/flashcards/config3.json" id="6" /> */}
-      {/* <Flowchart config="../widgets/flowchart/flowchart3.json" id="3" /> */}
     </>
   );
 }
